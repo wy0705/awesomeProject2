046d3d801b51d1df22fd91e24d06a98fa4ec7bf8
wy0705 <2652918156@qq.com>
2021-05-03 22:23:35 +0800

    222

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
+func a(){
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
+}
+
+func b()  {
+	for i := 0; i < 5; i++ {
+		defer func() {
+			fmt.Println(i)
+		}()
+	}
+}
 func main()  {
-	
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
 }
